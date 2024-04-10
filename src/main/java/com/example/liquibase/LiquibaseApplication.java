47fdf19e3100d85fceea2621c78619ff6f15c15a
karthikeyanyubi <karthikeyan.seethapathy@go-yubi.com>
2024-04-10 13:09:13 +0530

    final_change

--- src/main/java/com/example/liquibase/LiquibaseApplication.java
+++ src/main/java/com/example/liquibase/LiquibaseApplication.java
@@ -1,10 +1,17 @@
 package com.example.liquibase;
 
+import com.example.liquibase.models.Employee;
+import com.example.liquibase.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiquibaseApplication {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiquibaseApplication.class, args);
--- src/main/java/com/example/liquibase/models/Employee.java
+++ src/main/java/com/example/liquibase/models/Employee.java
@@ -2,10 +2,14 @@ package com.example.liquibase.models;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.NotFound;
-import org.hibernate.engine.jdbc.Size;
+
+import java.util.Date;
 
 
 @NoArgsConstructor
@@ -14,22 +18,53 @@ import org.hibernate.engine.jdbc.Size;
 @Setter
 @Entity
 @AllArgsConstructor
-@Table(name = "employee")
+@Table(name = "employee", uniqueConstraints = @UniqueConstraint(columnNames = {"employee_name", "phone_number"}))
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employee_id")
     private Long employeeId;
 
 
-    @Column(name = "employee_name")
+    @Column(name = "employee_name", nullable = false)
+    @NotBlank(message = "Employee name should be provided")
     private String employeeName;
 
-    @Column(name = "employee_designation")
+    @Column(name = "employee_designation", nullable = false)
+    @NotBlank(message = "Employee designation should be provided")
     private String employeeDesignation;
 
-    @Column(name = "phone_number")
+    @Column(name = "phone_number", nullable = false, length = 10)
+    @Size(min = 10, max = 10,message = "Phone number should have 10 numbers")
     private String phoneNumber;
 
+    @CreationTimestamp
+    @Column(name = "created_date", nullable = false, updatable = false)
+    Date createdDate;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "current_status")
+    private CampaignStatus currentStatus;
+
+    public enum CampaignStatus {
+        CAMPAIGN_CREATION_IN_PROGRESS,
+        CAMPAIGN_PLANNING_IN_PROGRESS,
+        SEGMENTATION_IN_PROGRESS,
+
+        CAMPAIGN_PLAN_SUBMITTED,
+        EXECUTION_IN_PROGRESS
+    }
+
+    @Column(columnDefinition = "BOOLEAN DEFAULT 'true'", name = "is_active")
+    public Boolean isActive;
+
+    private Integer rating;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = new Date();
+
+    }
 }
